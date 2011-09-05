d1788ac8367a4f551137eb8b97e36033155a33f4
Sven Mueller <sven.mueller@commercetools.de>
2011-09-05 11:30:55 +0200

    bla

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-var http = require('http'), sys = require('sys'),
-    rest = require('./restler');
+var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
-	rest.get('http://google.com').on('complete', function(data, res) {
-  		res.write(data);
-	});
+
 
   res.end('Hello World\nApp (ctbox2) is running..YAY!');
 }).listen(11757);
