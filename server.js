bb258332a40c4b3c2860a1849d7dde4e1ac89147
Sven Mueller <sven.mueller@commercetools.de>
2011-09-07 00:08:09 +0200

    bla

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-var http = require('http'), sys = require('sys'),
-    rest = require('restler');
+var sys = require('sys'),
+	http = require('http'),
+    rest = require('./restler');
 
-http.createServer(function (req, response) {
- 
-	rest.get('http://brita.testbackend.appspot.com/rest/products', {'headers': {'Authorization': 'Bearer 3fcf95ee-1db6-4079-a240-13980383647b'}}).on('complete', function(data) {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-  		response.write(data);
-		response.end('Hello World\nApp (ctbox2) is running..YAY!');
+renderProducts = function(response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write('<html><body>');
+	
+	rest.get('https://brita.testbackend.appspot.com/rest/products', {'headers':{'Authorization':'Bearer 3fcf95ee-1db6-4079-a240-13980383647b'}}).on('success', function(data) {
+		response.write('<ul>');
+		data.forEach(function(i) {
+			response.write('<li><a href="#">' + i.title + '</a></li>');
+		});
+		response.write('</ul>');
+	}).on('complete', function(data) {
+		response.end('</body></html>');
+	}).on('error', function(data) {
+		response.write('An error occured.');
+		response.end('</body></html>');
 	});
+}
 
+
+http.createServer(function (request, response) {	
+	renderProducts(response);		
 }).listen(11757);
