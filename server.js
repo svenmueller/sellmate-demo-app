dc7722531c3396972738718f8fa6fb924b69b7d1
Sven Mueller <sven.mueller@commercetools.de>
2011-09-07 03:32:56 +0200

    bla

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+/**
+ * Module dependencies.
+ */
 var sys = require('sys'),
+	express = require('express'),
 	http = require('http'),
     rest = require('./restler');
 
@@ -24,6 +28,83 @@ renderProducts = function(response) {
 	});
 }
 
+
+/*
 http.createServer(function (request, response) {	
 	renderProducts(response);		
 }).listen(11757);
+*/
+
+
+
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    locals: {
+      title: 'Express'
+    },
+	'products': products
+  });
+});
+
+app.get('/products/:id', function(req, res){
+	rest.get('https://brita.testbackend.appspot.com/rest/products/' + req.params.id, {'headers':{'Authorization':'Bearer 3fcf95ee-1db6-4079-a240-13980383647b'}}).on('complete', function(data) {
+		res.render('product', {
+	    locals: {
+	      title: data.title
+	    },
+		'product': data
+	  });
+	});
+});
+
+
+app.get('/products', function(req, res){
+	
+	rest.get('https://brita.testbackend.appspot.com/rest/products', {'headers':{'Authorization':'Bearer 3fcf95ee-1db6-4079-a240-13980383647b'}}).on('complete', function(data) {
+		res.render('index', {
+	    locals: {
+	      title: 'Products'
+	    },
+		'products': data
+	  });
+	});
+	
+});
+
+
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(11757);
+  console.log("Express server listening on port %d", app.address().port)
+}
