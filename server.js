4c8a5a6056a8a042b3c2ea7ac17e6313295b8c21
Sven Mueller <sven.mueller@commercetools.de>
2011-09-09 12:40:11 +0200

    bla

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.configure('development', function() {
 });
 
 app.configure('production', function() {
-  	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  	//app.use(express.errorHandler());
 	console.log("using config for 'production'"); 
 });
 
